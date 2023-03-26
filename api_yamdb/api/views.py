3b200d3a7fb71532f869810d8b1ba5acdf0acb0a
Михаил Хохлов <michelin_90@mail.ru>
2023-03-27 03:31:19 +0500

    bug fixed_1

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,7 +6,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status, views, viewsets, mixins, filters
 from rest_framework.decorators import action
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.tokens import RefreshToken
