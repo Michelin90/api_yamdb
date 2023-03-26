6eaf71ee7b9c47ac09a15a841a91ae0ab624b999
Daria0008 <46413906+Daria0008@users.noreply.github.com>
2023-03-26 13:46:50 +0500

    Update urls.py

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, CommentViewSet
+from .views import UserViewSet, CommentViewSet, CategoryViewSet
 
 app_name = 'api'
 
@@ -11,18 +11,6 @@ router_v1.register(
     r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
     CommentViewSet, basename='comments')
     
-urlpatterns = [
-    path('v1/', include(router_v1.urls)),
-]
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet
-
-app_name = 'api'
-
-router_v1 = DefaultRouter()
-router_v1.register(r'categories', CategoryViewSet, basename='categories')
-
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
 ]
